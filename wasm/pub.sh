1560240a8df4d930a4e82d182bcf96e88cc67428
Aleksandr Voronkov <avoronkov@plesk.com>
2022-06-02 23:15:22 +0700

    Set up pages publication pipeline

---
+++ wasm/pub.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -ex
+
+base_dir() {
+	dirname $(readlink -f $0)
+}
+
+die() {
+	echo "$@" >&2
+	exit 1
+}
+
+BASE_DIR="$(base_dir)"
+cd "$BASE_DIR" || dir "Failed to chdir '$BASE_DIR'"
+
+env GOOS=js GOARCH=wasm go build -o ./static/assets/demo.wasm ./cmd/wasm
+
+echo 'OK'
