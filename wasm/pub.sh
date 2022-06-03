cf16bb444f6ce715b6433aa0a6cc32fae3d1cd5f
Aleksandr Voronkov <avoronkov@plesk.com>
2022-06-04 03:02:17 +0700

    Fix pub.sh

--- wasm/pub.sh
+++ wasm/pub.sh
@@ -15,6 +15,7 @@ BASE_DIR="$(base_dir)"
 cd "$BASE_DIR" || dir "Failed to chdir '$BASE_DIR'"
 
 env GOOS=js GOARCH=wasm go build -o ./static/assets/demo.wasm ./cmd/wasm
+gzip -f -9 ./static/assets/demo.wasm
 cp -r ./static/assets ../public
 
 echo 'OK'
