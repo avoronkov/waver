c8fc2a03a137d93aef90f9b80528ca144fc30160
avoronkov <alxn1101r@gmail.com>
2022-06-04 07:55:56 +0000

    Deploying to gh-pages from @ avoronkov/waver@dbe279f208455c3f7e8a83748f22f787dd38e1f3 🚀

---
+++ index.js
@@ -0,0 +1,36 @@
+function initPage() {
+    loadDefaultCode();
+    loadDefaultInstruments();
+}
+
+const initGo = async () => {
+    const buffer = pako.ungzip(
+        await (await fetch("demo.wasm.gz")).arrayBuffer()
+    );
+    const go = new Go();
+    const result = await WebAssembly.instantiate(buffer, go.importObject);
+    go.run(result.instance);
+};
+// initGo();
+
+const updateCode = () => {
+    goPause(false);
+    const input = document.getElementById("code-story").value;
+    goPlay(input);
+};
+
+const loadDefaultCode = () => {
+    const code = goGetDefaultCode();
+    document.getElementById("code-story").value = code;
+};
+
+const updateInstruments = () => {
+    goPause(false);
+    const input = document.getElementById("inst-story").value;
+    goUpdateInstruments(input);
+};
+
+const loadDefaultInstruments = () => {
+    const inst = goGetDefaultInstruments();
+    document.getElementById("inst-story").value = inst;
+};
