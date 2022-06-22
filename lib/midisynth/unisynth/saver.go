9e96254d1542d09567b21f694052fc42251166d6
Aleksandr Voronkov <avoronkov@plesk.com>
2022-06-23 01:02:04 +0700

    Trim leading empty bytes in wav dump

--- lib/midisynth/unisynth/saver.go
+++ lib/midisynth/unisynth/saver.go
@@ -32,7 +32,8 @@ func (s *WavDataSaver) Read(data []byte) (int, error) {
 
 func (s *WavDataSaver) Close() error {
 	w := wav.CreateDefaultWav()
-	w.Data = &wav.DataBytes{Samples: s.buffer.Bytes()}
+	data := trimSamples(s.buffer.Bytes())
+	w.Data = &wav.DataBytes{Samples: data}
 
 	f, err := os.Create(s.file)
 	if err != nil {
@@ -41,4 +42,16 @@ func (s *WavDataSaver) Close() error {
 	defer f.Close()
 
 	return w.Write(f)
+}
+
+func trimSamples(data []byte) []byte {
+	i := 0
+	l := len(data)
+	for i+3 < l {
+		if !bytes.Equal(data[i:i+4], []byte{0, 0, 0, 0}) {
+			break
+		}
+		i += 4
+	}
+	return data[i:]
 }
