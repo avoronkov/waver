321a020ee80f469cf9b48e2d3440255591bb2ccb
avoronkov <alxn1101r@gmail.com>
2022-09-25 09:14:44 +0000

    Deploying to gh-pages from @ avoronkov/waver@3bc2506448d14945c91db4844710de1028d48ba2 🚀

---
+++ app-worker.js
@@ -0,0 +1,117 @@
+const cacheName = "app-" + "afb318c6f3059e98ff43ba9cdc66bd3480c06287";
+const resourcesToCache = ["/waver","/waver/app.css","/waver/app.js","/waver/manifest.webmanifest","/waver/wasm_exec.js","/waver/web/addon/mode/simple.js","/waver/web/app.wasm","/waver/web/init-codemirror.js","/waver/web/lib/codemirror.css","/waver/web/lib/codemirror.js","https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css","https://storage.googleapis.com/murlok-github/icon-192.png","https://storage.googleapis.com/murlok-github/icon-512.png"];
+
+self.addEventListener("install", (event) => {
+  console.log("installing app worker afb318c6f3059e98ff43ba9cdc66bd3480c06287");
+
+  event.waitUntil(
+    caches
+      .open(cacheName)
+      .then((cache) => {
+        return cache.addAll(resourcesToCache);
+      })
+      .then(() => {
+        self.skipWaiting();
+      })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  console.log("app worker afb318c6f3059e98ff43ba9cdc66bd3480c06287 is activated");
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("push", (event) => {
+  if (!event.data || !event.data.text()) {
+    return;
+  }
+
+  const notification = JSON.parse(event.data.text());
+  if (!notification) {
+    return;
+  }
+
+  const title = notification.title;
+  delete notification.title;
+
+  if (!notification.data) {
+    notification.data = {};
+  }
+  let actions = [];
+  for (let i in notification.actions) {
+    const action = notification.actions[i];
+
+    actions.push({
+      action: action.action,
+      path: action.path,
+    });
+
+    delete action.path;
+  }
+  notification.data.goapp = {
+    path: notification.path,
+    actions: actions,
+  };
+  delete notification.path;
+
+  event.waitUntil(self.registration.showNotification(title, notification));
+});
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+
+  const notification = event.notification;
+  let path = notification.data.goapp.path;
+
+  for (let i in notification.data.goapp.actions) {
+    const action = notification.data.goapp.actions[i];
+    if (action.action === event.action) {
+      path = action.path;
+      break;
+    }
+  }
+
+  event.waitUntil(
+    clients
+      .matchAll({
+        type: "window",
+      })
+      .then((clientList) => {
+        for (var i = 0; i < clientList.length; i++) {
+          let client = clientList[i];
+          if ("focus" in client) {
+            client.focus();
+            client.postMessage({
+              goapp: {
+                type: "notification",
+                path: path,
+              },
+            });
+            return;
+          }
+        }
+
+        if (clients.openWindow) {
+          return clients.openWindow(path);
+        }
+      })
+  );
+});
