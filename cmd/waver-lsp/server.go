02022f427701beab88d2f6f704a5be4f0006f35b
Alexander Voronkov <alxn1101r@gmail.com>
2025-01-15 01:15:49 +0300

    Completion for sample file pathes

--- cmd/waver-lsp/server.go
+++ cmd/waver-lsp/server.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"fmt"
-	"io"
+	"io/fs"
 	"log/slog"
-	"os"
-	"strings"
 
 	"github.com/avoronkov/waver/lib/seq/syntaxgen"
+	"github.com/avoronkov/waver/static"
 	"github.com/tliron/commonlog"
 	"github.com/tliron/glsp"
 	protocol "github.com/tliron/glsp/protocol_3_16"
@@ -45,7 +44,7 @@ func (s *Server) Shutdown(context *glsp.Context) error {
 }
 
 func (s *Server) TextDocumentDidChange(context *glsp.Context, params *protocol.DidChangeTextDocumentParams) error {
-	slog.Info("TextDocumentDidChange", "context", context, "params", params)
+	// slog.Info("TextDocumentDidChange", "context", context, "params", params)
 	return nil
 }
 
@@ -61,41 +60,53 @@ func (s *Server) TextDocumentCompletion(context *glsp.Context, params *protocol.
 	slog.Info("TDC", "context.Params", context.Params, "params", params)
 	slog.Info(fmt.Sprintf("Params: %v (%v)", params.PartialResultToken, params.WorkDoneProgressParams))
 
-	fpath := params.TextDocument.URI
-	if strings.HasPrefix(fpath, "file://") {
-		fpath = fpath[6:]
-	}
-	f, err := os.Open(fpath)
-	if err != nil {
-		slog.Error("Open file", "file", fpath, "error", err)
-		return nil, nil
-	}
-	defer f.Close()
+	// complete pragmas
+	completionItems = append(completionItems, s.completePragmas()...)
 
-	data, err := io.ReadAll(f)
-	if err != nil {
-		slog.Error("ReadAll", "error", err)
-		return nil, nil
-	}
-	lines := strings.Split(string(data), "\n")
-
-	line := lines[params.Position.Line]
-
-	slog.Info("Line", "string", line, "line", params.Position.Line, "character", params.Position.Character)
-
-	if strings.HasPrefix(line, "%") {
-		// complete pragmas
-		for _, pragma := range s.params.Pragmas {
-			item := pragma
-			detail := "[pragma]"
-			completionItems = append(completionItems, protocol.CompletionItem{
-				Label:      item,
-				Detail:     &detail,
-				InsertText: &item,
-			})
-		}
-	}
+	// complete sample files
+	completionItems = append(completionItems, s.completeSampleFiles()...)
 
 	slog.Info("Completion", "items", completionItems)
 	return completionItems, nil
+}
+
+func (s *Server) completePragmas() (items []protocol.CompletionItem) {
+	for _, pragma := range s.params.Pragmas {
+		item := pragma
+		// detail := "[pragma]"
+		kind := protocol.CompletionItemKindProperty
+		items = append(items, protocol.CompletionItem{
+			Label: item,
+			// Detail:     &detail,
+			InsertText: &item,
+			Kind:       &kind,
+		})
+	}
+	return items
+}
+
+func (s *Server) completeSampleFiles() (items []protocol.CompletionItem) {
+	subdir := "samples"
+	subdirlen := len(subdir) + 1
+	err := fs.WalkDir(static.Files, subdir, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if d.IsDir() {
+			return nil
+		}
+		kind := protocol.CompletionItemKindFile
+
+		items = append(items, protocol.CompletionItem{
+			Label: path[subdirlen:],
+			// Detail:     &detail,
+			Kind: &kind,
+		})
+		slog.Info("Sample", "file", path)
+		return nil
+	})
+	if err != nil {
+		slog.Error("WalkDir failed", "error", err)
+	}
+	return items
 }
